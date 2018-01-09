1dd03deea08b478dd72654f5970b64ac89b436f0
Blush <zupanibla@gmail.com>
2018-01-09 23:46:43 +0100

    Fix circular imports, cleanup

--- Sandbox/states/linefollowingstate.py
+++ Sandbox/states/linefollowingstate.py
@@ -1,12 +1,12 @@
 from lib.machinestate import MachineState
-from states.sharpturnstate import SharpTurnState
 from providers.motorpairprovider import motor_pair as mp
 from providers.linesensortripletprovider import line_sensors
 ls, ms, rs = line_sensors
 
+import states.sharpturnstate
+
 SPEED = 200
 THRESHOLD = 50
-
 
 class LineFollowingState(MachineState):
     def run(self):
@@ -20,8 +20,6 @@ class LineFollowingState(MachineState):
             elif state == (0, 0, 1):
                 mp.set_speed(SPEED / 2, +SPEED / 2)
             elif state == (1, 1, 0):
-                return SharpTurnState(-1)
+                return states.sharpturnstate.SharpTurnState(-1)
             elif state == (0, 1, 1):
-                return SharpTurnState(+1)
-
-        return None
+                return states.sharpturnstate.SharpTurnState(+1)

df269751eda16fc26438ddd3f338d1ee6109313c
Blush <zupanibla@gmail.com>
2018-01-10 19:21:07 +0100

    Fix random stopping after sharp turn

--- Sandbox/states/linefollowingstate.py
+++ Sandbox/states/linefollowingstate.py
@@ -7,19 +7,26 @@ import states.sharpturnstate # fixes circular import issues
 
 SPEED = 200
 THRESHOLD = 50
+STEERING = 0.7
 
 class LineFollowingState(MachineState):
     def run(self):
-        while True:
-            state = tuple([(0, 1)[r.value() < THRESHOLD] for r in (ls, ms, rs)])
+        state = (0, 1, 0)
 
-            if   state == (0, 1, 0):
+        while True:
+            readings = tuple([(0, 1)[r.value() < THRESHOLD] for r in (ls, ms, rs)])
+
+            if readings not in ((0, 0, 0), (1, 1, 1)):
+                state = readings
+
+            if state == (0, 1, 0): 
                 mp.set_speed(SPEED)
             elif state == (1, 0, 0):
-                mp.set_speed(SPEED / 2, -SPEED / 2)
+                mp.set_speed(SPEED / 2, -SPEED * STEERING)
             elif state == (0, 0, 1):
-                mp.set_speed(SPEED / 2, +SPEED / 2)
+                mp.set_speed(SPEED / 2, +SPEED * STEERING)
             elif state == (1, 1, 0):
                 return states.sharpturnstate.SharpTurnState(-1)
             elif state == (0, 1, 1):
                 return states.sharpturnstate.SharpTurnState(+1)
+

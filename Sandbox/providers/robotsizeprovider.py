a907917a4d7f78d8e375e40c1616ca7016b14cdc
SFJMG <jernej.grlj@gmail.com>
2018-01-11 20:28:00 +0100

    Change robotsize and fix the deg_to_motorpair_deg

--- Sandbox/providers/robotsizeprovider.py
+++ Sandbox/providers/robotsizeprovider.py
@@ -2,7 +2,7 @@ from math import pi
 
 #all units in cm
 WHEEL_RADIUS = 2.8
-AXLE_LENGHT = 11.8
+AXLE_LENGHT = 15.0
 SENSOR_TO_SENSOR = 2.5
 SENSOR_TO_AXLE = 5.7
 
@@ -10,5 +10,5 @@ def distance_to_motor_rot(d): # d in cm
 	return (d/WHEEL_RADIUS)*(180/pi)
 
 def deg_to_motorpair_deg(fi): # fi in deg motors rotating in opposite direction
-	return (AXLE_LENGHT/WHEEL_RADIUS)/(fi*4)
+	return (AXLE_LENGHT/WHEEL_RADIUS)*(fi/2)
 	

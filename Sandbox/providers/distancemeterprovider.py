3344b3f1dd3c0311a1c15bbc9b00799f70d236cf
SFJMG <jernej.grlj@gmail.com>
2018-01-11 22:00:55 +0100

    Removed pointless tuple

--- Sandbox/providers/distancemeterprovider.py
+++ Sandbox/providers/distancemeterprovider.py
@@ -2,4 +2,4 @@ from ev3dev.ev3 import UltrasonicSensor as us
 
 from lib.deviceproviders import provide_ev3_device
 
-front_distance_meter = tuple(provide_ev3_device(us, user='front facing ultrasonic sensor'))
+front_distance_meter = provide_ev3_device(us, user='front facing ultrasonic sensor')

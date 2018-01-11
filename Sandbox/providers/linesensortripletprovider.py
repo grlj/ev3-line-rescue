24e7495d8c14ca8fd556c7d5d41b3824317cebb6
Blush <zupanibla@gmail.com>
2018-01-11 21:18:45 +0100

    Remove measurements from prod

--- Sandbox/providers/linesensortripletprovider.py
+++ Sandbox/providers/linesensortripletprovider.py
@@ -4,6 +4,9 @@ from lib.deviceproviders import provide_ev3_devices
 from ev3dev.ev3 import ColorSensor as CS
 from lib.measurements import hooked_sensor_class
 
+Sensor = CS
+
+# comment in production
 Sensor = hooked_sensor_class(CS)
 
 line_sensors = tuple(provide_ev3_devices(Sensor, count=3, user='line sensor triplet'))

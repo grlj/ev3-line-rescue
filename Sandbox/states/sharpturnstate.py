dbf6ee9d4acdd411e15951c0b27acba1e06bfb42
SFJMG <jernej.grlj@gmail.com>
2018-01-11 21:59:51 +0100

    done some testing

--- Sandbox/states/sharpturnstate.py
+++ Sandbox/states/sharpturnstate.py
@@ -10,7 +10,7 @@ import states.linefollowingstate # fixes circular import issues
 
 SPEED = 160
 THRESHOLD = 50
-RIGHT_ANGLE = 90
+RIGHT_ANGLE = 85
 
 class SharpTurnState(MachineState):
     def __init__(self, turn):
@@ -18,17 +18,21 @@ class SharpTurnState(MachineState):
 
     def run(self):
         mp.reset()
+        lc()
+        mp.reset()
         mp.run_to_lr(dtr(SENSOR_TO_AXLE), dtr(SENSOR_TO_AXLE), SPEED)
         mp.block()
-        #mp.set_speed(-SPEED, self.turn * SPEED)
-        #wait_until(lambda: ms.value() < 70)
-        #mp.stop()
-        #mp.reset()
+       # mp.set_speed(-SPEED, self.turn * SPEED)
+       # wait_until(lambda: ms.value() < THRESHOLD)
+       # mp.stop()
+       # mp.reset()
         mp.run_to_lr(self.turn*detmd(RIGHT_ANGLE), -self.turn*detmd(RIGHT_ANGLE), SPEED)
         mp.block()
         mp.reset()
-        #mp.run_to_lr(dtr(SENSOR_TO_SENSOR), dtr(SENSOR_TO_SENSOR), SPEED)
-        #mp.block()
+        mp.run_to_lr(dtr(SENSOR_TO_SENSOR), dtr(SENSOR_TO_SENSOR), SPEED)
+        mp.block()
         lc()
+        mp.block()
+        mp.run_to_lr(-dtr(1.5), -dtr(1.5), SPEED)
         mp.block()
         return states.linefollowingstate.LineFollowingState()

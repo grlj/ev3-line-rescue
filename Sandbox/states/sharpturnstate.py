21634e510725cf72e31a376ce4cf5124113e9b76
Blush <zupanibla@gmail.com>
2018-01-11 21:17:22 +0100

    Add leds :)

--- Sandbox/states/sharpturnstate.py
+++ Sandbox/states/sharpturnstate.py
@@ -1,11 +1,11 @@
-from util.robotutils import wait_until
+from util.robotutils import wait_until, set_led_color
 from lib.machinestate import MachineState
 from providers.motorpairprovider import motor_pair as mp
 from providers.linesensortripletprovider import line_sensors
 from providers.robotsizeprovider import distance_to_motor_rot as dtr, SENSOR_TO_SENSOR, SENSOR_TO_AXLE
 ls, ms, rs = line_sensors
 
-import states.linefollowingstate # fixes circular import issues
+import states.linefollowingstate  # fixes circular import issues
 
 SPEED = 160
 THRESHOLD = 50
@@ -15,6 +15,8 @@ class SharpTurnState(MachineState):
         self.turn = turn
 
     def run(self):
+        set_led_color('amber')
+
         mp.reset()
         mp.run_to_lr(dtr(SENSOR_TO_AXLE), dtr(SENSOR_TO_AXLE), SPEED)
         mp.block()

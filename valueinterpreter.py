1de3699153ac51e9bab6cdb6b130af7a016cbed6
SFJMG <jernej.grlj@gmail.com>
2018-03-14 22:12:13 +0100

    fixed a major bugs

--- valueinterpreter.py
+++ valueinterpreter.py
@@ -10,11 +10,12 @@ class ValueInterpreter:
 		for i in range(len(values)): # constructs interpreted
 			if values[i] > THRESHOLD:
 				self.over_max += 1
+				if i in [0, 1]:
+					self.over_max_left += 1
+				elif i in [6, 7]:
+					self.over_max_right += 1
 			elif values[i] < MINTRESHOLD:
 				self.under_min += 1
-			if i in [0, 1]:
-				self.over_max_left += 1
-			elif i in [6, 7]:
-				self.over_max_right += 1
 
-vlaue_interpreter = ValueInterpreter
+
+value_interpreter = ValueInterpreter()

1ee0f379e59c43f04390b5ba0cf09f282c9d8667
Blush <zupanibla@gmail.com>
2018-03-14 19:24:49 +0100

    idk

--- e2.py
+++ e2.py
@@ -1,5 +1,6 @@
-from manoeuvres.centralizefront import centralize_front as cf, fine_centralize as fc, skipping as sk
+from manoeuvres.centralize import centralize as c, fine_centralize_lsa as fc, centralize_lsa as cl, centralize_axis as ca
 from devices import driver as d, line_sensor_array as lsa
+from valueinterpreter import value_interpreter as vi
 
 
 def s():

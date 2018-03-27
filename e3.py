e1d5e5f5b0fb00e874f67fd9d4576b56b08cbf5c
SFJMG <jernej.grlj@gmail.com>
2018-03-27 20:37:13 +0200

    some minor fixes and tests

--- e3.py
+++ e3.py
@@ -4,10 +4,10 @@ from valueinterpreter import value_interpreter
 from robotspecs import robot_specs as rs
 from crossings import crossing
 
-THRESHOLD = 90
-FORWARD_SPEED = 10
-TURNING_SPEED = 6
-SHARP_TURN = rs["circumference"]/16
+THRESHOLD = 75
+FORWARD_SPEED = 6
+TURNING_SPEED = 3
+SHARP_TURN = rs["circumference"]/4
 
 def line_follow():
 	while 1:
@@ -15,39 +15,50 @@ def line_follow():
 		direction = 1
 		index = value.index(max(value))
 		value_interpreter.push(values=value)
-		detected_green = tuple([i.is_green() for i in cs])
+		#detected_green = tuple([i.is_green() for i in cs])
 
 		if index in [0, 1, 2]:
 			direction = -1
 		elif index in [5, 6, 7]:
 			index = 7 - index
 
-		if detected_green[0] or detected_green[1]:
-			driver.stop()
-			driver.reset()
-			driver.lr(0.5, 0.5, 0.5)
-			driver.block()
-			crossing()
+		# if detected_green[0] or detected_green[1]:
+		# 	driver.stop()
+		# 	driver.reset()
+		# 	driver.lr(0.5, 0.5, 0.5)
+		# 	driver.block()
+		# 	crossing()
 
-		elif value_interpreter.under_min == 8:
+		if value_interpreter.under_min == 8:
 			pass # no line detected
+			value = lsa.values()
 		elif value_interpreter.over_max > 2:
-			if value_interpreter.over_max_left > 0 and value_interpreter.over_max_right > 0:
-				pass # crossing while going throu
-			elif value_interpreter.over_max_left > 0 and (value[3] > THRESHOLD or value[4] > THRESHOLD): # left sharp turn
+			# if value_interpreter.over_max_left > 0 and value_interpreter.over_max_right > 0:
+			# 	pass # crossing while going throu
+			if value_interpreter.over_max_left > 0 and (value[3] > THRESHOLD or value[4] > THRESHOLD) and value_interpreter.over_max_left > value_interpreter.over_max_right: # left sharp turn
 				driver.stop()
 				driver.reset()
 				driver.lr(-SHARP_TURN, SHARP_TURN, TURNING_SPEED)
-				print("left sharpturn")
-			elif value_interpreter.over_max_right > 0 and (value[3] > THRESHOLD or value[4] > THRESHOLD): # right sharp turn
+				driver.block()
+				driver.reset()
+				driver.lr(1, 1, FORWARD_SPEED)
+				driver.block()
+				value = lsa.values()
+			elif value_interpreter.over_max_right > 0 and (value[3] > THRESHOLD or value[4] > THRESHOLD) and value_interpreter.over_max_right > value_interpreter.over_max_left: # right sharp turn
 				driver.stop()
 				driver.reset()		
 				driver.lr(SHARP_TURN, -SHARP_TURN, TURNING_SPEED)
-				print("right sharpturn")
+				driver.block()
+				driver.reset()
+				driver.lr(1, 1, FORWARD_SPEED)
+				driver.block()
+				value = lsa.values()
 		elif (value[3] + value[4]) / 2 > THRESHOLD: # line centred
 			driver.set_speed(FORWARD_SPEED)
+			value = lsa.values()
 		else: # line uncentred
 			driver.set_speed(FORWARD_SPEED*((index)/3), direction*TURNING_SPEED*(1/(index+1)))
+			value = lsa.values()
 
 def stop():
 	driver.stop()

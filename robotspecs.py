f4ab4fe4e4b373a66f6204300c8278de57f0eb95
SFJMG <jernej.grlj@gmail.com>
2018-03-27 20:36:18 +0200

    updated to new robot

--- robotspecs.py
+++ robotspecs.py
@@ -1,9 +1,8 @@
 robot_specs = { # [cm]
 	        'wheel radius': 2.8,
-	         'axle length': 13.0,
-	'line sensor interval': 2.5,
-	       'line sensor y': 3.5,
-	      'color sensor y': 8.0,
+	         'axle length': 12.0,
+	       'line sensor y': 0.4,
+	      'color sensor y': 4.7,
 	    'distance meter y': 7.4,
-	       'circumference': 40.8,
+	       'circumference': 37.7,
 }

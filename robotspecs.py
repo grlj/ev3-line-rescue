4a73cb7b4ed85fcbd840e54a11068f105b6b58d6
Blush <zupanibla@gmail.com>
2018-03-28 16:30:18 +0200

    Resolve conflict

--- robotspecs.py
+++ robotspecs.py
@@ -1,16 +1,10 @@
 robot_specs = { # [cm]
 	        'wheel radius': 2.8,
 	         'axle length': 12.0,
-<<<<<<< HEAD
-	# 'line sensor interval': 2.5,
 	       'line sensor y': 0.4,
 	      'color sensor y': 4.7,
-	    # 'distance meter y': 7.4,
-	       'circumference': 12.0*3.14,
-=======
 	       'line sensor y': 0.4,
 	      'color sensor y': 4.7,
 	    'distance meter y': 7.4,
 	       'circumference': 37.7,
->>>>>>> c3ef697bf4551b214982c2a3dd38389338a9fb09
 }

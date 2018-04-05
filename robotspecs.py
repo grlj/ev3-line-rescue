38f8b019ef18034a48ff6b5a7f9e28035b816309
Blush <zupanibla@gmail.com>
2018-04-05 22:41:10 +0200

    Update rs format

--- robotspecs.py
+++ robotspecs.py
@@ -1,10 +1,19 @@
-robot_specs = { # [cm]
+robot_specs = { # tole bom deprecateal, ker je bs. glej spodaj
 	        'wheel radius': 2.8,
 	         'axle length': 12.0,
 	       'line sensor y': 0.4,
 	      'color sensor y': 4.7,
-	       'line sensor y': 0.4,
 	      'color sensor y': 4.7,
 	    'distance meter y': 7.4,
 	       'circumference': 37.7,
 }
+
+ # [cm]
+wheel_radius     = 2.8
+axle_length      = 12.0
+line_sensor_y    = 0.4
+color_sensor_y   = 4.7
+color_sensor_y   = 4.7
+distance_meter_y = 7.4
+circumference    = 37.7
+lsa_width        = 7.0  # TODO

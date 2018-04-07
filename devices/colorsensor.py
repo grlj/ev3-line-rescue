568288ffcaecf52cf49033f43b4936cb0f5afb27
SFJMG <jernej.grlj@gmail.com>
2018-04-07 23:11:16 +0200

    move to rgb-raw

--- devices/colorsensor.py
+++ devices/colorsensor.py
@@ -4,12 +4,16 @@ class ColorSensor(ColorSensor):
 	def __init__(self, port):
 		super(ColorSensor, self).__init__(port)
 
+	def values(self):
+		return (self.value(i) for i in range(3))
+
 	def calibrate(self):
-		self.green = self.raw
+		self.mode = 'RGB-RAW'
+		self.Green = self.values()
 
 	def is_green(self):
-		current_value = self.raw
-		if [abs(x - y) < 30 for x, y in zip(self.green, current_value)] == [True, True, True]:
+		current_value = self.values()
+		if [abs(x - y) < 30 for x, y in zip(self.Green, current_value)] == [True, True, True]:
 			return True
 		else:
 			return False

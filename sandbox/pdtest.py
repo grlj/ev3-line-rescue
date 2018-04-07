2eb5a20080c32dee1e0c350c1eb7c48f79d19aed
SFJMG <jernej.grlj@gmail.com>
2018-04-07 17:41:25 +0200

    add crossing handling to pdtest

--- sandbox/pdtest.py
+++ sandbox/pdtest.py
@@ -1,10 +1,10 @@
-from devices import line_sensor_array as lsa, driver as d
+from devices import line_sensor_array as lsa, driver as d, color_sensors as cs
 from components.linedataparser import line_data_parser as ldp
 from robotspecs import circumference
 from util.linedatamanipulation import error_from_vector
 from components.pdcontroller import PDController as PD
 from sandbox.devtools import rc, s, l, run_safe
-
+from sandbox.crossings import crossing as cross
 
 S = 4
 pd = PD(0.6, 3, 5)
@@ -15,9 +15,13 @@ def main():
 	d.reset()
 
 	while 1:
-
+		detected_green = [i.is_green() for i in cs]
 		raw = lsa.values()
 		ldp.push(raw)
+
+		if detected_green[0] or detected_green[1]:
+			cross()
+			pd.reset()
 
 		if (ldp.repeat_count):
 			if (ldp.repeat_count > 10):

55744e1c81deca671dfceff74d09d783eea2023e
SFJMG <jernej.grlj@gmail.com>
2018-04-23 20:57:08 +0200

    testing and optimizing

--- sandbox/crossings.py
+++ sandbox/crossings.py
@@ -1,11 +1,11 @@
 from devices import driver, color_sensors as cs, line_sensor_array as lsa
 from robotspecs import robot_specs as rs
 
-SHARP_TURN = rs["circumference"]/8
+SHARP_TURN = rs["circumference"]/4
 FORWARD_SPEED = 10
 TURNING_SPEED = 6
 THRESHOLD = 80
-FORWARD_DISTANCE = 1.5
+FORWARD_DISTANCE = 2
 
 def crossing():
 	driver.stop()
@@ -27,33 +27,33 @@ def crossing():
 
 	line_sensor_value = lsa.values()
 
-	if line_sensor_value[3] > THRESHOLD or line_sensor_value[4] > THRESHOLD:
-		if left > 8:
-			driver.lr(rs["color sensor y"], rs["color sensor y"], FORWARD_SPEED)
-			driver.lr(-SHARP_TURN, SHARP_TURN, TURNING_SPEED)
-			driver.lr(FORWARD_DISTANCE, FORWARD_DISTANCE, FORWARD_SPEED)
+	# if line_sensor_value[3] > THRESHOLD or line_sensor_value[4] > THRESHOLD:
+	if left > 8:
+		driver.lr(rs["color sensor y"], rs["color sensor y"], FORWARD_SPEED)
+		driver.lr(-SHARP_TURN, SHARP_TURN, TURNING_SPEED)
+		driver.lr(FORWARD_DISTANCE, FORWARD_DISTANCE, FORWARD_SPEED)
 
-		elif right > 8:
-			driver.lr(rs["color sensor y"], rs["color sensor y"], FORWARD_SPEED)
-			driver.lr(SHARP_TURN, -SHARP_TURN, TURNING_SPEED)
-			driver.lr(FORWARD_DISTANCE, FORWARD_DISTANCE, FORWARD_SPEED)
+	elif right > 8:
+		driver.lr(rs["color sensor y"], rs["color sensor y"], FORWARD_SPEED)
+		driver.lr(SHARP_TURN, -SHARP_TURN, TURNING_SPEED)
+		driver.lr(FORWARD_DISTANCE, FORWARD_DISTANCE, FORWARD_SPEED)
 
-		elif back > 8:
-			driver.lr(rs["color sensor y"], rs["color sensor y"], FORWARD_SPEED)
-			driver.lr(2*SHARP_TURN, -2*SHARP_TURN, TURNING_SPEED)
+	# elif back > 8:
+	# 	driver.lr(rs["color sensor y"], rs["color sensor y"], FORWARD_SPEED)
+	# 	driver.lr(2*SHARP_TURN, -2*SHARP_TURN, TURNING_SPEED)
 
-	if line_sensor_value[0] > THRESHOLD or line_sensor_value[1] > THRESHOLD or line_sensor_value[2] > THRESHOLD:
-		if left > 8:
-			driver.lr(rs["color sensor y"], rs["color sensor y"], FORWARD_SPEED)
-			driver.lr(-SHARP_TURN, SHARP_TURN, TURNING_SPEED)
-			driver.lr(FORWARD_DISTANCE, FORWARD_DISTANCE, FORWARD_SPEED)
+	# if line_sensor_value[0] > THRESHOLD or line_sensor_value[1] > THRESHOLD or line_sensor_value[2] > THRESHOLD:
+	# 	if left > 8:
+	# 		driver.lr(rs["color sensor y"], rs["color sensor y"], FORWARD_SPEED)
+	# 		driver.lr(-SHARP_TURN, SHARP_TURN, TURNING_SPEED)
+	# 		driver.lr(FORWARD_DISTANCE, FORWARD_DISTANCE, FORWARD_SPEED)
 
 
-	elif line_sensor_value[7] > THRESHOLD or line_sensor_value[6] > THRESHOLD or line_sensor_value[5] > THRESHOLD:
-		if right > 8:
-			driver.lr(rs["color sensor y"], rs["color sensor y"], FORWARD_SPEED)
-			driver.lr(SHARP_TURN, -SHARP_TURN, TURNING_SPEED)
-			driver.lr(FORWARD_DISTANCE, FORWARD_DISTANCE, FORWARD_SPEED)
+	# elif line_sensor_value[7] > THRESHOLD or line_sensor_value[6] > THRESHOLD or line_sensor_value[5] > THRESHOLD:
+	# 	if right > 8:
+	# 		driver.lr(rs["color sensor y"], rs["color sensor y"], FORWARD_SPEED)
+	# 		driver.lr(SHARP_TURN, -SHARP_TURN, TURNING_SPEED)
+	# 		driver.lr(FORWARD_DISTANCE, FORWARD_DISTANCE, FORWARD_SPEED)
 
 def scan_lr():
 	driver.stop()
